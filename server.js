0f51ed5bb0473764e4f7359d7f641540b192424e
geekhijabi <bimpeadetunji@rocketmail.com>
2019-01-06 14:35:02 +0100

    heroku build

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.resolve('./dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
+console.log('served up on port 8080')
